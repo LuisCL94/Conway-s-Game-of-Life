a38af7475b2b2ea81a54e5ada35907010cdaa68d
Luis Claudio Telles Lima <lclaudio.tl@gmail.com>
2019-07-19 00:01:48 -0300

    Refactored code, added information panel and implemented functionality to switch game colors

--- GameOfLife.java
+++ GameOfLife.java
@@ -2,24 +2,38 @@ import javax.swing.*;
 import javax.swing.event.*; 
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.border.LineBorder;
 
 public class GameOfLife implements ActionListener, ChangeListener, Runnable {
 
 	private JFrame frame = new JFrame();
-	private GamePanel panelGame = new GamePanel();
+	private JLayeredPane layeredPane = new JLayeredPane();
+	private JPanel gamePanel = new JPanel();
+	private GameWindow gameWindow = new GameWindow();
 	private JComboBox cellsComboBox = new JComboBox();
 	private JButton nextButton = new JButton();
 	private JButton play_stop_button = new JButton();
 	private JButton cleanButton = new JButton();
 	private JLabel speedIconLabel = new JLabel();
-	private JSlider speedSlider = new JSlider(1, 5);
+	private JSlider speedSlider = new JSlider(1, 6);
 	private JLabel speedLabel = new JLabel();
 	private JLabel sizeIconLabel = new JLabel();
 	private JSlider sizeSlider = new JSlider(2, 20);
 	private JLabel sizeLabel = new JLabel();
 	private JLabel countGenLabel = new JLabel();
+	private JButton infoButton = new JButton();
+	private JPanel infoPanel = new JPanel();
+	private JButton backButton = new JButton("BACK");
+	private JButton invertButton = new JButton("");
 	
 	boolean running = false;
+	
+	public void switchPanels(JPanel panel) {
+		layeredPane.removeAll();
+		layeredPane.add(panel);
+		layeredPane.repaint();
+		layeredPane.revalidate();
+	}
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -35,14 +49,29 @@ public class GameOfLife implements ActionListener, ChangeListener, Runnable {
 
 	public GameOfLife() {
 
-		panelGame.setSpeed(3);
-		panelGame.setCellSize(15);
-
 		frame.setTitle("Conway`s Game of Life");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(923, 720);
+		frame.setSize(1000, 720);
 		frame.getContentPane().setLayout(null);
-		frame.getContentPane().add(panelGame);
+		layeredPane.setLocation(0, 0);
+		layeredPane.setSize(1000, 683);
+		frame.getContentPane().add(layeredPane);
+		layeredPane.setLayout(new CardLayout(0, 0));
+		
+		layeredPane.add(gamePanel, "name_34924502800590");
+		gamePanel.setLayout(null);
+		
+		gameWindow.setBounds(10, 10, 901, 601);
+		gameWindow.setSpeed(4);
+		gameWindow.setCellSize(15);
+		gameWindow.setAliveCellColor(Color.BLACK);
+		gameWindow.setDeadCellColor(Color.WHITE);
+
+		gameWindow.setBackground(gameWindow.getDeadCellColor());
+		gamePanel.add(gameWindow);
+	
+		speedSlider.setValue(gameWindow.getSpeed());
+		sizeSlider.setValue(gameWindow.getCellSize());
 	
 		String[] cells = {"Random","Create Cell","Gosper Glider Gun", "Glider", 
 			"Lightweight Spaceship", "10 Cell Row", "Tumbler", "Diehard", "Acorn"};
@@ -60,14 +89,12 @@ public class GameOfLife implements ActionListener, ChangeListener, Runnable {
 		
 		speedIconLabel.setIcon(new ImageIcon("Icons/speed1.png"));
 		speedIconLabel.setBounds(400, 620, 60, 48);
-		speedSlider.setValue(panelGame.getSpeed());
 		speedSlider.setBounds(442, 628, 100, 16);
 		speedLabel.setText("speed game = " + speedSlider.getValue());
 		speedLabel.setBounds(440, 643, 140, 15);
 
 		sizeIconLabel.setIcon(new ImageIcon("Icons/grid1.png"));
 		sizeIconLabel.setBounds(627, 620, 41, 48);
-		sizeSlider.setValue(panelGame.getCellSize());
 		sizeSlider.setBounds(658, 628, 150, 16);
 		sizeLabel.setText("cell/grid size = 15");
 		sizeLabel.setBounds(668, 643, 140, 15);
@@ -75,24 +102,42 @@ public class GameOfLife implements ActionListener, ChangeListener, Runnable {
 		countGenLabel.setText("0"); //set initial value 
 		countGenLabel.setBounds(871, 626, 70, 30);
 
+		infoButton.setIcon(new ImageIcon("Icons/info2.png"));
+		infoButton.setBounds(935, 193, 41, 60);
+
 		cellsComboBox.addActionListener(this);
 		nextButton.addActionListener(this);
 		play_stop_button.addActionListener(this);
 		cleanButton.addActionListener(this);
 		speedSlider.addChangeListener(this);
 		sizeSlider.addChangeListener(this);
+		infoButton.addActionListener(this);
+		backButton.addActionListener(this);
+		invertButton.addActionListener(this);
 
-		frame.getContentPane().add(cellsComboBox);
-		frame.getContentPane().add(nextButton);
-		frame.getContentPane().add(play_stop_button);
-		frame.getContentPane().add(cleanButton);
-		frame.getContentPane().add(speedIconLabel);
-		frame.getContentPane().add(speedSlider);		
-		frame.getContentPane().add(speedLabel);
-		frame.getContentPane().add(sizeIconLabel);
-		frame.getContentPane().add(sizeSlider);
-		frame.getContentPane().add(sizeLabel);
-		frame.getContentPane().add(countGenLabel);
+		gamePanel.add(cellsComboBox);
+		gamePanel.add(nextButton);
+		gamePanel.add(play_stop_button);
+		gamePanel.add(cleanButton);
+		gamePanel.add(speedIconLabel);
+		gamePanel.add(speedSlider);		
+		gamePanel.add(speedLabel);
+		gamePanel.add(sizeIconLabel);
+		gamePanel.add(sizeSlider);
+		gamePanel.add(sizeLabel);
+		gamePanel.add(countGenLabel);
+		gamePanel.add(infoButton);
+		
+		invertButton.setBounds(934, 400, 45, 48);
+		invertButton.setIcon(new ImageIcon(getClass().getResource("Icons/invert1.png")));
+		
+		gamePanel.add(invertButton);
+		
+		layeredPane.add(infoPanel, "name_42708651229050");
+		infoPanel.setLayout(null);
+		backButton.setBounds(332, 646, 76, 25);
+		
+		infoPanel.add(backButton);
 		
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
@@ -100,44 +145,45 @@ public class GameOfLife implements ActionListener, ChangeListener, Runnable {
 
 	int x = 0;
 	int y = 0;
+
 	public void actionPerformed(ActionEvent e) {		
 		if(e.getSource().equals(cellsComboBox)) {
 
 				if(cellsComboBox.getSelectedItem() == "Create Cell") {
 					x=0;
 					countGenLabel.setText(Integer.toString(x));
-					panelGame.createCell();
+					gameWindow.createCell();
 				}
 
 				else if(cellsComboBox.getSelectedItem() == "Gosper Glider Gun") {
 					x=0;
 					countGenLabel.setText(Integer.toString(x));
-					panelGame.gliderGun();
+					gameWindow.gliderGun();
 				}
 
 				else if(cellsComboBox.getSelectedItem() == "Glider") {
 					x=0;
 					countGenLabel.setText(Integer.toString(x));
-					panelGame.glider();
+					gameWindow.glider();
 				}
 
 				else if(cellsComboBox.getSelectedItem() == "Lightweight Spaceship") {
 					x=0;
 					countGenLabel.setText(Integer.toString(x));					
-					panelGame.lightweightSpaceship();
+					gameWindow.lightweightSpaceship();
 				}	
 				else if(cellsComboBox.getSelectedItem() == "10 Cell Row") {
 					x=0;
 					countGenLabel.setText(Integer.toString(x));	
-					panelGame.tenCellRow();
+					gameWindow.tenCellRow();
 				}
 		}
 
 		if(e.getSource().equals(nextButton)) {
 				x++;
 				countGenLabel.setText(Integer.toString(x));
-				panelGame.nextGeneration();
-				panelGame.repaint();
+				gameWindow.nextGeneration();
+				gameWindow.repaint();
 		}
 		
 		if(e.getSource().equals(play_stop_button)) {		
@@ -157,22 +203,35 @@ public class GameOfLife implements ActionListener, ChangeListener, Runnable {
 		if(e.getSource().equals(cleanButton)) {
 				x=0;
 				countGenLabel.setText(Integer.toString(x));	
-				panelGame.cleanGamePanel();		
+				gameWindow.cleanGameWindow();		
 		}
-	
+		
+		if(e.getSource().equals(infoButton)) 
+			switchPanels(infoPanel);
+		
+		if(e.getSource().equals(backButton)) 
+			switchPanels(gamePanel);
+
+		if(e.getSource().equals(invertButton)) {
+			Color aliveColor = gameWindow.getAliveCellColor();
+			gameWindow.setAliveCellColor(gameWindow.getDeadCellColor());
+			gameWindow.setDeadCellColor(aliveColor);
+			gameWindow.setBackground(aliveColor);
+			gameWindow.repaint();
+		}		
 	}
 
 	public void stateChanged(ChangeEvent e) {
 		
 		if(sizeSlider.getValueIsAdjusting()) {
-			panelGame.setCellSize(sizeSlider.getValue());
+			gameWindow.setCellSize(sizeSlider.getValue());
 			sizeLabel.setText("cell/grid  size = " + sizeSlider.getValue());
-			panelGame.repaint();
+			gameWindow.repaint();
 		}
 		if(speedSlider.getValueIsAdjusting()) {
-			panelGame.setSpeed(speedSlider.getValue());
+			gameWindow.setSpeed(speedSlider.getValue());
 			speedLabel.setText("speed game = " + speedSlider.getValue());
-			panelGame.repaint();
+			gameWindow.repaint();
 		}
 	}
 
@@ -180,14 +239,15 @@ public class GameOfLife implements ActionListener, ChangeListener, Runnable {
 		while(running == true) {
 			x++;
 			countGenLabel.setText(Integer.toString(x));
-			panelGame.nextGeneration();
-			panelGame.repaint();	
+			gameWindow.nextGeneration();
+			gameWindow.repaint();	
 			try{
-				Thread.sleep(panelGame.SpeedGame(panelGame.getSpeed())); //15, 40, 65, 79, 101, 123, 150, 172, 194, 216 
+				Thread.sleep(gameWindow.speedGame(gameWindow.getSpeed())); 
 			}catch(	Exception ex) {
 				ex.printStackTrace();
 			}
 		}
 	}
+	
 
 }
